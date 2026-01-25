3e756c573062636edbcd735768a8ed56e7eb2ef0
dschuchi <dschuchi@fi.uba.ar>
2026-01-25 15:52:11 -0300

    Posibilidad de configurar el orden de los botones de TableActions

--- src/components/TableActions.vue
+++ src/components/TableActions.vue
@@ -1,55 +1,69 @@
 <template>
   <div class="d-flex ga-2 justify-end align-center">
 
-    <slot name="prepend" :id="id"></slot>
+    <template v-for="actionItem in order" :key="actionItem">
 
-    <v-tooltip v-if="canSoftDelete" :text="isActive ? 'Desactivar' : 'Restaurar'" location="top">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon variant="text" size="small" :color="isActive ? 'error' : 'success'"
-          @click="$emit('softDelete', id)">
-          <v-icon :icon="isActive ? 'mdi-delete' : 'mdi-delete-restore'" />
-        </v-btn>
-      </template>
-    </v-tooltip>
+      <v-tooltip v-if="actionItem === 'soft-delete' && canSoftDelete" :text="isActive ? 'Desactivar' : 'Restaurar'"
+        location="top">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" icon variant="text" size="small" :color="isActive ? 'error' : 'success'"
+            @click="$emit('soft-delete', id)">
+            <v-icon :icon="isActive ? 'mdi-delete' : 'mdi-delete-restore'" />
+          </v-btn>
+        </template>
+      </v-tooltip>
 
-    <v-tooltip v-if="canHardDelete" text="Eliminar definitivamente" location="top">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon variant="text" size="small" color="error" @click="$emit('hardDelete', id)">
-          <v-icon icon="mdi-delete-forever" />
-        </v-btn>
-      </template>
-    </v-tooltip>
+      <v-tooltip v-if="actionItem === 'hard-delete' && canHardDelete" text="Eliminar definitivamente" location="top">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" icon variant="text" size="small" color="error" @click="$emit('hard-delete', id)">
+            <v-icon icon="mdi-delete-forever" />
+          </v-btn>
+        </template>
+      </v-tooltip>
 
-    <v-tooltip v-if="canEdit" text="Editar" location="top">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon variant="text" size="small" color="primary" @click="$emit('edit', id)">
-          <v-icon icon="mdi-pencil" />
-        </v-btn>
-      </template>
-    </v-tooltip>
+      <v-tooltip v-if="actionItem === 'edit' && canEdit" text="Editar" location="top">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" icon variant="text" size="small" color="primary" @click="$emit('edit', id)">
+            <v-icon icon="mdi-pencil" />
+          </v-btn>
+        </template>
+      </v-tooltip>
 
-    <v-tooltip v-if="canView" text="Ver detalles" location="top">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon variant="text" size="small" color="info" @click="$emit('view', id)">
-          <v-icon icon="mdi-eye" />
-        </v-btn>
-      </template>
-    </v-tooltip>
+      <v-tooltip v-if="actionItem === 'view' && canView" text="Ver detalles" location="top">
+        <template v-slot:activator="{ props }">
+          <v-btn v-bind="props" icon variant="text" size="small" color="info" @click="$emit('view', id)">
+            <v-icon icon="mdi-eye" />
+          </v-btn>
+        </template>
+      </v-tooltip>
 
-    <slot name="append" :id="id"></slot>
+      <slot v-if="!isStandardAction(actionItem)" :name="actionItem" :id="id" :is-active="isActive"></slot>
+
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import type { PropType } from 'vue'
+
+const props = defineProps({
   id: { type: [Number, String], required: true },
   isActive: { type: Boolean, default: true },
 
   canEdit: { type: Boolean, default: true },
   canView: { type: Boolean, default: true },
   canSoftDelete: { type: Boolean, default: true },
-  canHardDelete: { type: Boolean, default: false }
+  canHardDelete: { type: Boolean, default: false },
+
+  order: {
+    type: Array as PropType<string[]>,
+    default: () => ['view', 'edit', 'soft-delete', 'hard-delete']
+  }
 })
 
-defineEmits(['view', 'edit', 'softDelete', 'hardDelete'])
+defineEmits(['view', 'edit', 'soft-delete', 'hard-delete'])
+
+const isStandardAction = (name: string) => {
+  return ['view', 'edit', 'soft-delete', 'hard-delete'].includes(name)
+}
 </script>
