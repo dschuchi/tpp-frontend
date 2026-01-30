d915ff6d6de6ae3e111e1a0a1c595fe13d6f21b1
dschuchi <dschuchi@fi.uba.ar>
2026-01-30 10:03:09 -0300

    Agrega la ruta de pagina no encontrada

---
+++ src/pages/[...path].vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-empty-state icon="mdi-map-search-outline" headline="404 - Página no encontrada"
+      title="Parece que te has perdido">
+      <template #text>
+        <div class="text-medium-emphasis mb-4">
+          La ruta <strong>{{ $route.path }}</strong> no existe en este sistema.
+        </div>
+      </template>
+
+      <template #actions>
+        <v-btn to="/" color="primary" variant="flat">
+          Volver al Inicio
+        </v-btn>
+      </template>
+    </v-empty-state>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+definePage({
+  meta: {
+    permission: '*',
+  }
+})
+</script>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...path]': RouteRecordInfo<'/[...path]', '/:path(.*)', { path: ParamValue<true> }, { path: ParamValue<false> }>,
     '/403': RouteRecordInfo<'/403', '/403', Record<never, never>, Record<never, never>>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
     '/roles/': RouteRecordInfo<'/roles/', '/roles', Record<never, never>, Record<never, never>>,
@@ -41,6 +42,10 @@ declare module 'vue-router/auto-routes' {
   export interface _RouteFileInfoMap {
     'src/pages/index.vue': {
       routes: '/'
+      views: never
+    }
+    'src/pages/[...path].vue': {
+      routes: '/[...path]'
       views: never
     }
     'src/pages/403.vue': {
