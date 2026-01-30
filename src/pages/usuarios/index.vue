fede42bd368e6030e99fd870bd387630e97e18cc
dschuchi <dschuchi@fi.uba.ar>
2026-01-30 11:54:39 -0300

    Crea la ruta de gestion de usuarios

---
+++ src/pages/usuarios/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <PageHeader title="Usuarios" subtitle="Administra los usuarios del sistema."></PageHeader>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts" setup>
+import PageHeader from '@/components/PageHeader.vue';
+
+definePage({
+  meta: {
+    permission: '*'
+  }
+})
+</script>
