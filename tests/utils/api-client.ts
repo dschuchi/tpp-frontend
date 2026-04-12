67fb3717cbec2664507bc0b190a3a12791730d62
dschuchi <dschuchi@fi.uba.ar>
2026-04-12 12:56:05 -0300

    tests: mejora en las pruebas del ABM de clientes

---
+++ tests/utils/api-client.ts
@@ -0,0 +1,23 @@
+import { request, APIRequestContext } from '@playwright/test';
+import { USERS_ENDPOINTS } from '../../src/api/endpoints';
+
+export async function getAuthContext(): Promise<APIRequestContext> {
+  const loginContext = await request.newContext();
+
+  const response = await loginContext.post(`${process.env.VITE_API_BASE_URL}${USERS_ENDPOINTS.LOGIN}`, {
+    data: {
+      email: 'ev@gmail.com',
+      password: 'admin'
+    }
+  });
+
+  const { accessToken } = await response.json();
+
+  return await request.newContext({
+    baseURL: process.env.VITE_API_BASE_URL,
+    extraHTTPHeaders: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Accept': 'application/json',
+    },
+  });
+}
